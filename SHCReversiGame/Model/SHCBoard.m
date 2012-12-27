9d358d62e2d261719066b3b0c85614ef50b7ca6d
Colin Eberhardt <ceberhardt@scottlogic.co.uk>
2012-12-27 22:12:44 +0000

    Unit tests fixed

--- SHCReversiGame/Model/SHCBoard.m
+++ SHCReversiGame/Model/SHCBoard.m
@@ -23,13 +23,22 @@
 
 - (BoardCellState)cellStateAtColumn:(NSInteger)column andRow:(NSInteger)row
 {
+    [self checkBoundsForColumn:column andRow:row];
     return _board[column][row];
 }
 
 - (void)setCellState:(BoardCellState)state forColumn:(NSInteger)column andRow:(NSInteger)row
 {
+    [self checkBoundsForColumn:column andRow:row];
     _board[column][row] = state;
 }
+
+- (void)checkBoundsForColumn:(NSInteger)column andRow:(NSInteger)row
+{
+    if (column < 0 || column > 7 || row < 0 || row > 7)
+        [NSException raise:NSRangeException format:@"row or column out of bounds"];
+}
+
 
 - (void)clearBoard
 {
